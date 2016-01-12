c77efb15da09d0720defae24cd1811f03fbe8d76
shuboyang <shuboyang@yhgenomics.com>
2016-01-12 17:34:17 +0800

    add 3rd lib

--- linux_build.sh
+++ linux_build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf cmakebuild
-CMaker project:meloton src:*.cpp,*.cc,*.hpp head:*.h lib:./lib/libprotobuf.a,./lib/libuv.a
+CMaker project:meloton src:*.cpp,*.cc,*.hpp head:*.h lib:./lib/libprotobuf.a,./lib/libuv.a,./lib/libmaraton.a
 cd cmakebuild
-#cmake ./
-#make
+cmake ./
+make -j 4
